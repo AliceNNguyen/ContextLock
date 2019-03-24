79b16882a8e73ff838d4973056c734b18fda69e6
Alice Nguyen <alice.nguyen@campus.lmu.de>
2019-03-24 22:49:00 +0100

    add files

---
+++ app/src/main/java/com/example/alicenguyen/contextlock/fingerprint/FingerPrintListener.java
@@ -0,0 +1,20 @@
+//package com.amirarcane.lockscreen.fingerprint;
+package com.example.alicenguyen.contextlock.fingerprint;
+
+
+/**
+ * Created by Arcane on 7/11/2017.
+ */
+
+public interface FingerPrintListener {
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onError(CharSequence errorString);
+
+    void onHelp(CharSequence helpString);
+
+}
+
